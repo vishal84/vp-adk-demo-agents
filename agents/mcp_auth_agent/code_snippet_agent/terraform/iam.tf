bf6ffd266b2f9f5a84476f9988475495e562ecdf
Vishal Patel <5624846+vishal84@users.noreply.github.com>
2025-12-03 15:15:26 -0500

    Update iam.tf

--- agents/mcp_auth_agent/code_snippet_agent/terraform/iam.tf
+++ agents/mcp_auth_agent/code_snippet_agent/terraform/iam.tf
@@ -1,20 +1,20 @@
 # Creating a service account
-resource "google_service_account" "service_account" {
+resource "google_service_account" "code_snippet_agent_sa" {
   account_id   = var.service_account_name
   display_name = var.service_account_display_name
   project      = var.gcp_project_id
 }
 
 # Granting the role of Cloud Run Invoker to the service account
-resource "google_project_iam_member" "invoker" {
+resource "google_project_iam_member" "run_invoker" {
   project = var.gcp_project_id
   role    = "roles/run.invoker"
-  member  = "serviceAccount:${google_service_account.service_account.email}"
+  member  = "serviceAccount:${google_service_account.code_snippet_agent_sa.email}"
 }
 
 # Granting the role of Logs Writer to the service account
 resource "google_project_iam_member" "writer" {
   project = var.gcp_project_id
   role    = "roles/logging.logWriter"
-  member  = "serviceAccount:${google_service_account.service_account.email}"
+  member  = "serviceAccount:${google_service_account.code_snippet_agent_sa.email}"
 }
