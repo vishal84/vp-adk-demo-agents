d21a2fe5fe84c720769fc9a4db12001ed84c62a9
Vishal Patel <5624846+vishal84@users.noreply.github.com>
2025-12-03 15:21:31 -0500

    Update variables.tf

--- agents/mcp_auth_agent/code_snippet_agent/terraform/variables.tf
+++ agents/mcp_auth_agent/code_snippet_agent/terraform/variables.tf
@@ -21,3 +21,8 @@ variable "service_account_display_name" {
 variable "service_name" {
   description = "The name of the Cloud Run service."
 }
+
+output "code_snippet_agent_service_account_email" {
+  description = "The email of the service account created for the code snippet agent."
+  value       = google_service_account.code_snippet_agent_sa.email
+}
